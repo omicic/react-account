4cf382c3efeadbd17499e4b850ed517c83215dcc
Olivera Micic <omicic@gmail.com>
2022-05-05 13:58:57 +0200

    final comitt

--- src/components/EditAccount/EditAccount.js
+++ src/components/EditAccount/EditAccount.js
@@ -1,13 +1,12 @@
 import React, { useState  } from "react";
 import { useNavigate, useParams} from "react-router-dom";
-import Account from "../Account/Account";
 
 function EditAccount(props) {
 
     const {id} = useParams()
     let navigate = useNavigate();
-
     const [accountEdit,setAccount] = useState(props.accounts.filter(acc => acc.id == id)[0])
+    
 
    //let accountEdit =  accounts.filter(acc => acc.id == id)[0];
 
@@ -15,7 +14,7 @@ function handleClick(e) {
     let index;
     let lenght =props.accounts.length
     for (let i = 0; i < lenght ; i++) {
-      if(props.accounts[i].id==accountEdit.id){
+      if(props.accounts[i].id===accountEdit.id){
          index = i;
       } 
   }
@@ -39,7 +38,7 @@ function handleClick(e) {
                </h3>
                <div className="row">
                <div className="col-10 offset-1">
-                   <input type="text" defaultValue={accountEdit.id} id="id" className="form-control"/><br/>
+                   <input type="text" defaultValue={accountEdit.id} className="form-control"/><br/>
                    <input type="text" onChange={e=>{setAccount({...accountEdit,name:e.target.value})}}  defaultValue={accountEdit.name} className="form-control"/><br/>
                    <input type="text" onChange={e=>{setAccount({...accountEdit,lastname:e.target.value})}} defaultValue={accountEdit.lastname} className="form-control"/><br/>
                    <input type="text" onChange={e=>{setAccount({...accountEdit,phone:e.target.value})}} defaultValue={accountEdit.phone} className="form-control"/><br/>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-//import "bootstrap/dist/css/bootstrap.css";
+import "./css/bootstrap/dist/css/bootstrap.css";
 //import "bootstrap/dist/css/bootstrap";
 
 //ReactDOM.render(<App />, document.querySelector("#root"));
